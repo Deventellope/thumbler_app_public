ec5ff462e4223eccdaf3ee2c77ac87eeae67d315
Deventellope <isheriff544@gmai.com>
2026-06-16 23:36:57 +0100

    removed ads,txt file and using just flask route

--- flask_app.py
+++ flask_app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/ads.txt')
 def ads_txt():
-    content = "google.com, pub-XXXXXXXXXXXXXXXX, DIRECT, f08c47fec0942fa0"
+    content = "google.com, pub-7839701645099353, DIRECT, f08c47fec0942fa0"
     return content, 200, {'Content-Type': 'text/plain'}
     # return send_from_directory('static', 'ads.txt')
 
